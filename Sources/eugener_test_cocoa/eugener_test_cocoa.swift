26e87d120eb512a03c91765288cbd2620cd06735
Eugene R <eugener@checkmarx.com>
2024-11-26 13:21:12 +0200

    4th

--- Sources/eugener_test_cocoa/eugener_test_cocoa.swift
+++ Sources/eugener_test_cocoa/eugener_test_cocoa.swift
@@ -1,2 +1,9 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+public class MyClass {
+    public init() {}
+
+    public func sayHello() -> String {
+        return "Hello from eugener_test_cocoa!"
+    }
+}
